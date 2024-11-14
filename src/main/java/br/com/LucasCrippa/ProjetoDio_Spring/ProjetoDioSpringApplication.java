189c35aa598e69bcd9df3385c47fc52fa074b45b
Lucas <lucas_crippa@hotmail.com>
2024-11-14 20:23:14 -0300

    First commmit

---
+++ src/main/java/br/com/LucasCrippa/ProjetoDio_Spring/ProjetoDioSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.LucasCrippa.ProjetoDio_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/LucasCrippa/ProjetoDio_Spring/ProjetoDioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LucasCrippa.ProjetoDio_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
